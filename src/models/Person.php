885312014edc02a52117ae32ad79ddd82b7a84ee
AiramLD <airamluisdiaz629@gmail.com>
2023-11-21 20:17:09 +0000

    Persin controler CR-D

---
+++ src/models/Person.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Airam\People\Models;
+
+use Illuminate\Database\Eloquent\Model;
+ 
+class Person extends Model {
+    //protected $table = 'Persons';
+}
---
+++ src/views/person/show.php
@@ -0,0 +1,3 @@
+<h1>Usuario con id =<?=$person->id?></h1>
+<p>Nombre: <?=$person->name?></p>
+<h3>Actualizado el <?=$person->update_at?></h3>
